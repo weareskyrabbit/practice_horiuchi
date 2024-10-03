8370148673d5a02a9931c2f02c880d3f30d254a3
tf-intern-horiuchi <y.horiuchi.p1@p1.techfirm.co.jp>
2024-10-03 16:46:08 +0900

    features.tsでPlyaer,GameStatusを定義、セルが空かどうかの関数定義

--- src/components/templates/ReversiGame/features.ts
+++ src/components/templates/ReversiGame/features.ts
@@ -1,3 +1,23 @@
+
+export enum Player {
+    White = '○',
+    Black = '●'
+    // [WIP] あとで変更？？
+}
+
+export interface GameState {
+    boardWidth: number;
+    boardData: string[];
+    currentPlayer: Player;
+    winner: Player | null;
+    draw: boolean;
+}
+
 export const reversiGameTitle = () => {
     return 'リバーシ';
+}
+
+// セルが空かどうかの関数定義
+export function isCellEmpty(gameState: GameState, index: number) {
+    return gameState.boardData[index] == '';
 }
