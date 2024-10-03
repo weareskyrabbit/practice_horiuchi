515b6e49d06243d32cdb742818b2dedbf673b61b
tf-intern-horiuchi <y.horiuchi.p1@p1.techfirm.co.jp>
2024-10-03 16:44:27 +0900

    _app.tsxをリバーシ用に上書き

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import { MarkGameProvider } from "@/providers/MarkGameProvider";
+// import { MarkGameProvider } from "@/providers/MarkGameProvider";
+import { ReversiGameProvider } from "@/providers/ReversiGameProvider";
 import "./globals.css";
 import type { AppProps } from "next/app";
 
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <MarkGameProvider>
+//       <Component {...pageProps} />
+//   </MarkGameProvider>;
+// }
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <MarkGameProvider>
+  return <ReversiGameProvider>
       <Component {...pageProps} />
-  </MarkGameProvider>;
+  </ReversiGameProvider>;
 }
