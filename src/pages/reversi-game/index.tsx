64a8fc25eb11c018a3f12e6a7d54b8f7d294ad76
tf-intern-horiuchi <y.horiuchi.p1@p1.techfirm.co.jp>
2024-10-03 11:54:39 +0900

    リバーシページのindex.tsx作成

---
+++ src/pages/reversi-game/index.tsx
@@ -0,0 +1,13 @@
+// リバーシ
+import { ReversiGame } from "@/components/templates/ReversiGame/ReversiGameTry";
+
+const ReversiGameT = () => {
+    return <>
+    <ReversiGame />
+    <br />
+    // トップページへ
+    <a href="../">トップページへ</a>
+    </>
+};
+
+export default ReversiGameT;
