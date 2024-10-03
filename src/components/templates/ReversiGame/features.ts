cb16464f6b475eacf35828998455be0c64b9bf98
tf-intern-horiuchi <y.horiuchi.p1@p1.techfirm.co.jp>
2024-10-03 17:56:13 +0900

    ボードデータを列ごとの配列に変換する関数の定義

--- src/components/templates/ReversiGame/features.ts
+++ src/components/templates/ReversiGame/features.ts
@@ -21,3 +21,20 @@ export const reversiGameTitle = () => {
 export function isCellEmpty(gameState: GameState, index: number) {
     return gameState.boardData[index] == '';
 }
+
+// ボードデータを列ごとの配列に変換する関数の定義
+export function convertReversiGameCols(boardWidth: number, boardData: string[]) {
+    // 各行の列ごとの値を格納した配列にする
+    // 列ごとの値を格納するための二次元配列 cols を初期化
+    var cols: Array<Array<string>> = [];
+    // 列のインデックスをループ
+    for (var colIdx = 0; colIdx < boardWidth; colIdx++) {
+        var col: Array<string> = [];
+        // 行のインデックスをループ
+        for (var rowIdx = 0; rowIdx < boardWidth; rowIdx++) {
+            col.push(boardData[colIdx * boardWidth + rowIdx]);
+        }
+        cols.push(col);
+    }
+    return cols;
+}
