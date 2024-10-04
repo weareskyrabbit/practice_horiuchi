17f93ba6299dcb6e2dafb54b3903cb160ed042a7
tf-intern-horiuchi <y.horiuchi.p1@p1.techfirm.co.jp>
2024-10-04 14:51:15 +0900

    ゲームルールの実装

---
+++ src/components/ormanisms/GameStatusR/GameStatusR.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useReversiGame, Player } from '@/providers/ReversiGameProvider';
+
+const GameStatusR: React.FC = () => {
+    const { gameState } = useReversiGame();
+    const { currentPlayer, winner, draw } = gameState;
+
+    return (
+        <div className="game-status">
+            <h2>ゲームの状態</h2>
+            {winner ? (
+                <h3>勝者: {winner === Player.Black ? '⚫' : '⚪'}</h3>
+            ) : draw ? (
+                <h3>引き分け</h3>
+            ) : (
+                <h3>現在のプレイヤー: {currentPlayer === Player.Black ? '⚫' : '⚪'}</h3>
+            )}
+        </div>
+    );
+};
+
+export default GameStatusR;
